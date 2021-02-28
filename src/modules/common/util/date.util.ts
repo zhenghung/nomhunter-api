69f91790bc6a153c02a647e95f39023a6ec1b426
Zheng Hung Chuah <zheng_hung@hotmail.com>
2021-02-28 14:46:20 +0000

    feat(leaderboard): added earliest date score was obtained for venue rankings

--- src/modules/common/util/date.util.ts
+++ src/modules/common/util/date.util.ts
@@ -1,12 +1,13 @@
 import { addYears, subYears } from "date-fns";
 import { Between } from "typeorm";
+import { FindOperator } from "typeorm/find-options/FindOperator";
 
 export class DateUtil {
-  public static afterDate(date: Date) {
+  public static afterDate(date: Date): FindOperator<Date> {
     return Between(date, addYears(date, 100));
   }
 
-  public static beforeDate(date: Date) {
+  public static beforeDate(date: Date): FindOperator<Date> {
     return Between(subYears(date, 100), date);
   }
 }
